b4abb3000264fa6b4b5832cf5d670061f27028bb
Harshit-dell <harshitsahu967@gmail.com>
2025-02-21 19:19:21 +0530

    Update Product.java
    Model was not matching with sql

--- src/main/java/com/telusko/ecom_proj/model/Product.java
+++ src/main/java/com/telusko/ecom_proj/model/Product.java
@@ -19,14 +19,14 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private String description;
+    private String desc;
     private String brand;
     private BigDecimal price;
     private String category;
 
     private Date releaseDate;
-    private boolean productAvailable;
-    private int stockQuantity;
+    private boolean Available;
+    private int Quantity;
 
     private String imageName;
     private String imageType;
